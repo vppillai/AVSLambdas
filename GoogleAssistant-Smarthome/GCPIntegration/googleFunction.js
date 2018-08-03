ffd63a9f137f66749e8808a587591c862a1041c8
Vysakh P Pillai <3634378+vppillai@users.noreply.github.com>
2018-08-03 19:19:52 +0530

    add support for multiple devices - assistant
    Add support for multiple devices and controlling them all together

--- GoogleAssistant-Smarthome/GCPIntegration/googleFunction.js
+++ GoogleAssistant-Smarthome/GCPIntegration/googleFunction.js
@@ -49,23 +49,34 @@ function listDevices(client, registryId, projectId, cloudRegion, callback) {
 }
 
 //Retrieve the given device from the registry.
-function getDevice(client, deviceId, registryId, projectId, cloudRegion, callback) {
+function getDevice(client, devices, registryId, projectId, cloudRegion, callback) {
     const parentName = `projects/${projectId}/locations/${cloudRegion}`;
     const registryName = `${parentName}/registries/${registryId}`;
-    const request = {
-        name: `${registryName}/devices/${deviceId}`
-    };
 
-    client.projects.locations.registries.devices.get(request, (err, data) => {
-        if (err) {
-            console.log('Could not find device:', deviceId);
-            console.log(err);
-        } else {
-            console.log('Found device:', deviceId);
-            //    console.log(data);
-            callback(data);
-        }
-    });
+    var devData = [];
+
+    for (i = 0; i < devices.length; i++) { //get all device infos
+        const request = {
+            name: `${registryName}/devices/${devices[i].id}`
+        };
+
+        client.projects.locations.registries.devices.get(request, (err, data) => {
+            if (err) {
+                // console.log('Could not find device:', devices[i].id);
+                console.log(err);
+            } else {
+                // console.log('Found device:', devices[i].id);
+                //    console.log(data);
+                devData.push(data);
+                devData = JSON.parse(JSON.stringify(devData)); //deepcopy
+
+                if (devData.length == devices.length) { //callback when all devices are available.
+                    console.log(devData)
+                    callback(devData);
+                }
+            }
+        });
+    }
 }
 
 //Returns an authorized API client by discovering the Cloud IoT Core API with
@@ -94,8 +105,7 @@ function getClient(cb) {
 // Send configuration data to device.
 function setDeviceConfig(
     client,
-    deviceId,
-    registryId,
+    devices,
     projectId,
     cloudRegion,
     data,
@@ -103,26 +113,35 @@ function setDeviceConfig(
     callback
 ) {
     const parentName = `projects/${projectId}/locations/${cloudRegion}`;
-    const registryName = `${parentName}/registries/${registryId}`;
 
     const binaryData = Buffer.from(data).toString('base64');
-    const request = {
-        name: `${registryName}/devices/${deviceId}`,
-        versionToUpdate: version,
-        binaryData: binaryData
-    };
 
-    client.projects.locations.registries.devices.modifyCloudToDeviceConfig(request,
-        (err, data) => {
-            if (err) {
-                console.log('Could not update config:', deviceId);
-                console.log('Message: ', err);
-                callback(false);
-            } else {
-                console.log('Success :', data);
-                callback(true);
-            }
-        });
+    var callbackCount = 0;
+
+    for (i = 0; i < devices.length; i++) {
+        const registryName = `${parentName}/registries/${devices[i].customData.registry}`;
+        deviceId = devices[i].id;
+        const request = {
+            name: `${registryName}/devices/${deviceId}`,
+            versionToUpdate: version,
+            binaryData: binaryData
+        };
+
+        console.log('setting config for : ',request.name);
+        client.projects.locations.registries.devices.modifyCloudToDeviceConfig(request,
+            (err, data) => {
+                callbackCount++;
+                if (err) {
+                    console.log('Could not update config:', deviceId);
+                    console.log('Message: ', err);
+                } else {
+                    console.log('Success :', data);
+                }
+                if (callbackCount == devices.length) {//got all callbacks
+                    callback(true); //blindy sending success irrespective of looking at actual data.
+                }
+            });
+    }
     // [END iot_set_device_config]
 }
 
@@ -152,41 +171,51 @@ function handleSync(res) {
 
 function getDevicesFromPartnerCloud(response, res) {
     //process device metadata
-    function processDeviceData(devData) {
-        var deviceTemplate = {
+    function processDeviceData(devDataArray) {
+        response = {
             "requestId": requestID,
             "payload": {
                 "agentUserId": "VnlzYWtoIFAgUGlsbGFp",
-                "devices": [
-                    {
-                        "id": devData.id,
-                        "type": "action.devices.types.LIGHT",
-                        "traits": ["action.devices.traits.OnOff"],
-                        "name": { "name": "" },
-                        "willReportState": false,
-                        "deviceInfo": {
-                            "manufacturer": "Microchip Technologies"
-                        },
-                        "customData": {
-                            "registry": registryId
-                        }
-                    }]
+                "devices": []
             }
         };
 
-        if (devData.metadata.name) {
-            deviceTemplate.payload.devices[0].name.name = devData.metadata.name;
-            console.log("deviceName: " + devData.metadata.name);
+        deviceTemplate = {
+            "type": "action.devices.types.LIGHT",
+            "traits": ["action.devices.traits.OnOff"],
+            "name": { "name": "" },
+            "willReportState": false,
+            "deviceInfo": {
+                "manufacturer": "Microchip Technologies"
+            },
+            "customData": {
+                "registry": registryId
+            }
         }
-        else {
-            deviceTemplate.payload.devices[0].name.name = devData.id;
-            console.log("we will call him " + devData.id);
+
+        var i;
+        for (i = 0; i < devDataArray.length; i++) {
+            //fill unique device ID
+            devData = devDataArray[i];
+
+            deviceTemplate.id = devData.id;
+            if (devData.metadata.name) {
+                deviceTemplate.name.name = devData.metadata.name;
+                console.log("deviceName: " + devData.metadata.name);
+            }
+            else {
+                deviceTemplate.name.name = devData.id;
+                console.log("we will call him " + devData.id);
+            }
+            if (devData.metadata.type) {
+                console.log("deviceName: " + devData.metadata.type);
+            }
+            response.payload.devices.push(deviceTemplate);
+            //deepcopy response
+            response = JSON.parse(JSON.stringify(response));
         }
-        if (devData.metadata.type) {
-            console.log("deviceName: " + devData.metadata.type);
-        }
-        console.log(JSON.stringify(deviceTemplate));
-        res.status(200).send(deviceTemplate);
+        console.log(JSON.stringify(response));
+        res.status(200).send(response);
 
     }
 
@@ -195,7 +224,7 @@ function getDevicesFromPartnerCloud(response, res) {
         const getDeviceCb = function (client) {
             getDevice(
                 client,
-                devices[0].id, //currently supporting only one device
+                devices, //currently supporting only one device
                 registryId,
                 projectId,
                 cloudRegion,
@@ -235,7 +264,6 @@ function handleExec(payload, res) {
             "requestId": requestID,
             "payload": {
                 "commands": [{
-                    "ids": [payload.commands[0].devices[0].id],
                     "status": "SUCCESS", //currently sending blanket success
                     "states": {
                         "on": payload.commands[0].execution[0].params.on,
@@ -244,6 +272,12 @@ function handleExec(payload, res) {
                 }]
             }
         }
+        execResponse.payload.commands[0].ids=[];
+
+        for(i=0;i<payload.commands[0].devices.length;i++){
+            execResponse.payload.commands[0].ids.push(payload.commands[0].devices[i].id);
+        }
+        
         console.log(JSON.stringify(execResponse));
         res.status(200).send(execResponse);
     }
@@ -257,8 +291,7 @@ function handleExec(payload, res) {
         }
         setDeviceConfig(
             client,
-            payload.commands[0].devices[0].id,
-            payload.commands[0].devices[0].customData.registry,
+            payload.commands[0].devices,
             projectId,
             cloudRegion,
             command,
@@ -270,6 +303,7 @@ function handleExec(payload, res) {
 }
 
 function handleQuery(payload, res) {
+    var deviceID = payload.devices[0].id;
     var deviceID = payload.devices[0].id;
     var queryResponse = {
         "requestId": requestID,
@@ -300,6 +334,7 @@ exports.helloWorld = (req, res) => {
             break;
         case "action.devices.EXECUTE":
             console.error("EXEC called");
+            console.log(req.body.inputs[0].payload);
             handleExec(req.body.inputs[0].payload, res);
             break;
     }
